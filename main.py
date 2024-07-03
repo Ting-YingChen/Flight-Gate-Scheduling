17b79fd342a38297d2f4e76e7f63f18d5b4e1ae6
Ting-Ying Chen <tingying.chen@tum.com>
2024-07-03 03:16:07 +0200

    Bring the new Instance.py from Arthur to update2. But I can't create data properly with Instance.py in line 21 of main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import instance_TYorganised
+import Instance
 import vertices_weights as vw
 import CPP_MIP as cpp
 # import FGS_MIP as fgs
@@ -12,21 +12,13 @@ def main():
     alpha3 = 100  # Penalty scaling factor for buffer time deficits
     t_max = 30
 
-
-    #jsdvbjs
-    # zlakejfhazekjhfzelkrjhfiojer
     # load excel file and create all relevant data structures
-    LOCAL_PATH = 'C:/Users/ge92qac/PycharmProjects/Flight-Gate-Scheduling/Brussels copy.xlsm'
+    LOCAL_PATH = '/Users/chentingying/Documents/tum/AS_Operation_Management/Brussels.xlsm'
     check_output = False
     # Import data
     flightsBrussels, gatesBrussels, T_timeDiff, Gates_N, Gates_D, num_flights, num_gates, Flight_No, Gate_No, ETA, ETD, P_preferences, \
         flights_to_activities, activities_to_flights, U_successor, M_validGate, shadow_constraints, no_towable_flights,\
-        gates_to_indices, indices_to_gates = instance_TYorganised.createInputData(LOCAL_PATH, check_output)
-
-
-    # Load and prepare data
-    # data = dp.load_data("path_to_your_data_file")
-    # processed_data = dp.preprocess_data(data)
+        gates_to_indices, indices_to_gates = Instance.createInputData(LOCAL_PATH, check_output)
 
     large_negative = vw.calculate_large_negative(Flight_No, num_flights, no_towable_flights, T_timeDiff, P_preferences, M_validGate, alpha1, alpha2, alpha3, t_max)
     weights = vw.get_weight_matrix(Flight_No, num_gates, T_timeDiff, P_preferences, U_successor, M_validGate, alpha1, alpha2, alpha3, t_max, large_negative,
@@ -34,12 +26,12 @@ def main():
     # Note: the keys of the dictionary 'weights' are exactly the names of all vertices present in the graph!
 
     print("large_negative:", large_negative)
-    # print("weights:", weights)
+    print("weights:", weights)
 
     # CPP Model
     # Build the model and retrieve the solution and cluster assignments
     cpp_solution = cpp.build_cpp_model(weights, shadow_constraints)
-    # print(cpp_solution) # why all 0???
+    print(cpp_solution)
 
     # FGS MIP Model
 
@@ -47,11 +39,7 @@ def main():
     # Heuristic Model
     # todo: adjust it so it works without 'vertices' list
     heuristic_solution = Heuristic.iterative_gate_optimization(weights, U_successor, M_validGate, P_preferences)
-
-    # Print or process the solutions
-    # print("CCP Solution:", ccp_solution)
-    # print("FGS Solution:", fgs_solution)
-    print("Heuristic Solution:", heuristic_solution)
+    # print("Heuristic Solution:", heuristic_solution)
 
 if __name__ == "__main__":
     main()
